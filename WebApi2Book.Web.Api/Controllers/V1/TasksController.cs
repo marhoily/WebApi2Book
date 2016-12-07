5909b203cfbbf8cd430232c2c2519c28ab73a2cd
mli2805 <mli2805@gmail.com>
2016-12-07 21:58:05 +0300

    api versioning started

---
+++ WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi2Book.Web.Common.Routing;
+
+namespace WebApi2Book.Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("tasks")]
+    public class TasksController : ApiController
+    {
+    }
+}
