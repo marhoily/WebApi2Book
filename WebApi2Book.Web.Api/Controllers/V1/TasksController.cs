2d66cc6fb3f033516f5daa68d5deef6a396df654
mli2805 <mli2805@gmail.com>
2016-12-08 11:10:08 +0300

    Versioning chapter is finished

--- WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using WebApi2Book.Web.Api.Models;
 using System.Net.Http;
 using System.Web.Http;
 using WebApi2Book.Web.Common.Routing;
@@ -11,5 +8,14 @@ namespace WebApi2Book.Web.Api.Controllers.V1
     [ApiVersion1RoutePrefix("tasks")]
     public class TasksController : ApiController
     {
+        [Route("", Name = "AddTaskRoute")]
+        [HttpPost]
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {
+            return new Task
+            {
+                Subject = "In v1, newTask.Subject = " + newTask.Subject
+            };
+        }
     }
 }
