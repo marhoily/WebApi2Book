afa6b4ff9f2da8ab63f452cec82f033acb62d9ed
mli2805 <mli2805@gmail.com>
2016-12-09 14:42:04 +0300

    Unit of Work chapter is done.

--- WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -10,6 +10,7 @@ using Ninject.Web.Common;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Logging;
 using WebApi2Book.Data.SqlServer.Mapping;
+using WebApi2Book.Web.Common;
 
 namespace WebApi2Book.Web.Api
 {
@@ -42,6 +43,7 @@ namespace WebApi2Book.Web.Api
             .BuildSessionFactory();
             container.Bind<ISessionFactory>().ToConstant(sessionFactory);
             container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+            container.Bind<IActionTransactionHelper>().To<ActionTransactionHelper>().InRequestScope();
         }
         private ISession CreateSession(IContext context)
         {
